af4735b8675b25aadec8e771276e4fe222bbb4b3
ITSPaul <powell.paul@itsligo.ie>
2018-02-20 16:59:20 +0000

    started wpf client

---
+++ WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebAPIAuthenticationClient;
+
+namespace WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PlayerAuthentication.baseWebAddress = "http://localhost:50574/";
+            IEnumerable<GameScoreObject> list = PlayerAuthentication.getScores(4, "Battle Call")as IEnumerable<GameScoreObject>;
+            this.listBox.ItemsSource = list;
+            
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
