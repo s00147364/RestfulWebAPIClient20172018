ad4c56eb005cc15d865bcba4f5073fd20717484c
ppowell <ppowell@G0030-03>
2018-02-22 09:53:08 +0000

    Implemented Repository Pattern

--- WpfClient/GameMVVM/GamePlayerViewModel.cs
+++ WpfClient/GameMVVM/GamePlayerViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebAPIAuthenticationClient;
+using WpfClient.DAL;
 
 namespace WpfClient.GameMVVM
 {
@@ -13,13 +14,15 @@ namespace WpfClient.GameMVVM
     {
         
         public ObservableCollection<GameScoreObject> ScoreList { get; set; }
-
+        IGameScore<GameScoreObject> context;
         public GamePlayerViewModel()
         {
+            context = new GameScoreRepository();
             if(DesignerProperties.GetIsInDesignMode(
                 new System.Windows.DependencyObject())) return;
             PlayerAuthentication.baseWebAddress = "http://localhost:50574/";
-            ScoreList = new ObservableCollection<GameScoreObject>( PlayerAuthentication.getScores(4, "Battle Call"));
+            //ScoreList = new ObservableCollection<GameScoreObject>( PlayerAuthentication.getScores(4, "Battle Call"));
+            ScoreList = new ObservableCollection<GameScoreObject>(context.getGameScores(4, "Battle Call"));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
